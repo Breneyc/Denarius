3e0c83f0a069deab4bbebe54f7315e7b8e3f14d8
Anderson <202100183@upqroo.edu.mx>
2024-02-14 14:52:56 -0500

    Name

--- lib/core/presentation/views/PaymentBalance.dart
+++ lib/core/presentation/views/PaymentBalance.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 class PaymentBalance extends StatelessWidget {
   const PaymentBalance({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
